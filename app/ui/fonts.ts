56756d0291326f8106564373badda7c1c22f7972
racodin <skkim6.dev@gmail.com>
2024-06-18 11:54:52 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto_Mono } from 'next/font/google';
+
+export const inter = Inter({ 
+  subsets: ['latin'] 
+});
+
+export const lusitana = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
